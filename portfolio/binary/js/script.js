d8025e623e68742a9d1db25ccfa19ad2f5957b71
AspenWeb <52273165+dev-vector@users.noreply.github.com>
2025-06-04 17:24:43 +0300

    Add files via upload

---
+++ portfolio/binary/js/script.js
@@ -0,0 +1,10 @@
+//Скрипт для првильного отображения (без скролов) в IE
+
+// $(window).resize(function (event) {
+// 	mainblock();
+// });
+// function mainblock() {
+// 	var h = $(window).outerHeight();
+// 	$('.main-block').css('min-height', h);
+// }
+// mainblock();
